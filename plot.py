1189ab735ed0f82f5d5ca49fa8e30a75c62cd4d2
BredeFannemel <brede.fannemel@gmail.com>
2024-09-03 12:33:00 +0200

    Create plot.py

---
+++ plot.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+class Plot(tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.parent = parent
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.fig, self.ax = plt.subplots(figsize=(5, 4))
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self)
+        self.canvas.get_tk_widget().grid(row=0, column=0, padx=10, pady=10)
+
+    def update_histogram(self, weights):
+        self.ax.clear()
+        self.ax.hist(weights, bins=10, color='blue', edgecolor='black')
+        self.ax.set_title("Weight Distribution")
+        self.ax.set_xlabel("Weight")
+        self.ax.set_ylabel("Number of Fish")
+        self.canvas.draw()
